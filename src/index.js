28aaf589bb9f1eb8ddd0dd4435313eddc24bded4
Elias de Araújo <93089971+artxlias@users.noreply.github.com>
2022-03-04 00:54:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import BoardView from "./components/Board";
 import "./main.scss";
 import "./styles.scss";
 
+alert("Olá, seja bem-vindo ao 2048 animado! Utilize as setinhas do teclado (cima, baixo, esquerda, direita) para jogar e divirta-se!");
+
 const App = () => {
   return <BoardView />;
 };
